2105ab9ddd898caac2fd24e534d0425ed8aa29e8
loveelsa0626 <879759871@qq.com>
2016-10-18 14:34:20 +0800

    添加二级菜单实体类MiddleMenu

---
+++ src/entity/MiddleMenu.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class MiddleMenu {
+    private Integer id;
+    private String name;
+    private Integer largeid;
+    private Integer level;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLargeid() {
+        return largeid;
+    }
+
+    public void setLargeid(Integer largeid) {
+        this.largeid = largeid;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+}
